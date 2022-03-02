1a23703d4a73458388ea3acd86ffd3ab8bd04482
Michael Meoak <mmeoak240@gmail.com>
2022-03-02 11:36:29 -0700

    commit tree f7a558483c8c6c1ffeb4bd6cb1061ec875efdf12

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array, event) {
+let newObj = [];
+for (let i = 0; i < names.length; i++ ){
+ newObj[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+ debugger;
+}
+return newObj;
+}
+
+writeCards(names, "surprise")
+
+function countDown() {
+  for (let i = 10; i > -1; i--) {
+    console.log(i);
+  }
+}
